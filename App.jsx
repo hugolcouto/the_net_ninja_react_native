292c1335b2bc45e716710324fca68df61298cb6a
Hugo Couto <contato@hugocouto.dev>
2021-10-20 21:30:28 -0300

    Larning some components such as: View/SafeAreaView, Text, Button and TextInput

--- App.jsx
+++ App.jsx
@@ -1,17 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
+	Button,
 	SafeAreaView,
 	StatusBar,
+	StyleSheet,
+	Text,
+	TextInput,
+	View,
 } from 'react-native';
-import { TextComponentMock } from './src/components'
 
 const App = () => {
+	const [name, setName] = useState();
+
+	const buttonHandler = () => {
+
+	}
+
 	return (
-		<SafeAreaView>
-			<StatusBar />
-			<TextComponentMock />
+		<SafeAreaView style={styles.container}>
+			<Text style={styles.title} >Digite seu nome</Text>
+			<Text style={styles.title} >{name}</Text>
+			<TextInput
+				style={styles.textInput}
+				placeholder="Digite seu nome"
+				onChangeText={value => setName(value)}
+				value={name}
+				keyboardType="email-address"
+			/>
+			<View>
+				<Button title='Click me' color="#00b894" onPress={buttonHandler} />
+			</View>
 		</SafeAreaView>
 	);
 };
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+		backgroundColor: '#ced1ce'
+	},
+	title: {
+		fontSize: 50,
+		marginBottom: 20,
+	},
+	textInput: {
+		borderBottomWidth: 1,
+		borderBottomColor: '#00b894',
+		marginBottom: 20,
+		paddingBottom: 0,
+		width: '90%'
+	}
+})
 
 export default App;
